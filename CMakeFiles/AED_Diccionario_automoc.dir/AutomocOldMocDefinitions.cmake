f22eb78ddc514b28d55ba7d948783278a1cc1535
alrus2797 <alrus2797@gmail.com>
2016-10-08 15:07:03 -0500

    Dejar heaps2

--- CMakeFiles/AED_Diccionario_automoc.dir/AutomocOldMocDefinitions.cmake
+++
@@ -1,1 +0,0 @@
-set(AM_OLD_COMPILE_SETTINGS "QT_CORE_LIB;QT_GUI_LIB;QT_NO_DEBUG;vtkRenderingCore_AUTOINIT=2(vtkInteractionStyle,vtkRenderingOpenGL2) ~~~ /usr/local/include/vtk-7.0;/home/bryce/Documentos/proyectos/AED/AED-Traductor;/usr/include/qt4;/usr/include/qt4/QtGui;/usr/include/qt4/QtCore ~~~  ~~~ FALSE ~~~ ")
--- CMakeFiles/AED_Diccionario_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "AED_Diccionario_automoc.cpp"
-  "CMakeFiles/AED_Diccionario_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/AED_Diccionario_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
